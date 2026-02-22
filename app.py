90aed690dcfc30bafca265a4d157bfeb75c97f11
henry <chanclets29@gmail.com>
2026-02-22 15:36:20 -0600

    add gunicorn

--- app.py
+++ app.py
@@ -2,22 +2,23 @@ from flask import Flask, render_template, request, redirect
 import json
 import os
 from werkzeug.utils import secure_filename
+import cloudinary
+import cloudinary.uploader
 
 app = Flask(__name__)
 
-# contraseña admin
+# 🔐 contraseña admin
 PASSWORD = "1234"
 
-# archivo donde se guardan los productos
+# 📁 archivo donde se guardan los productos
 PRODUCTOS_FILE = "productos.json"
 
-# carpeta donde se guardan las imágenes
-UPLOAD_FOLDER = "static/uploads"
-
-# crear carpeta si no existe
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
-
+# 🔹 CONFIGURACIÓN CLOUDINARY (PON TUS DATOS AQUÍ)
+cloudinary.config(
+    cloud_name="dj8adnlhn",
+    api_key="395448994121461",
+    api_secret="_1VV3uJ3WOZjv02ZBal2Py5Q2ho"
+)
 
 # cargar productos
 def cargar_productos():
@@ -30,18 +31,15 @@ def cargar_productos():
     except:
         return []
 
-
 # guardar productos
 def guardar_productos(productos):
     with open(PRODUCTOS_FILE, "w", encoding="utf-8") as f:
         json.dump(productos, f, indent=4, ensure_ascii=False)
 
-
 @app.route("/")
 def index():
     productos = cargar_productos()
     return render_template("index.html", productos=productos)
-
 
 @app.route("/admin", methods=["GET", "POST"])
 def admin():
@@ -60,16 +58,17 @@ def admin():
             if archivo and archivo.filename != "":
                 nombre_archivo = secure_filename(archivo.filename)
 
-                ruta = os.path.join(UPLOAD_FOLDER, nombre_archivo)
+                # 🔥 SUBIR IMAGEN A CLOUDINARY
+                resultado = cloudinary.uploader.upload(archivo)
 
-                archivo.save(ruta)
+                url_imagen = resultado["secure_url"]
 
                 productos = cargar_productos()
 
                 productos.append({
                     "nombre": nombre,
                     "precio": precio,
-                    "imagen": f"uploads/{nombre_archivo}"
+                    "imagen": url_imagen
                 })
 
                 guardar_productos(productos)
@@ -77,7 +76,6 @@ def admin():
             return redirect("/")
 
     return render_template("admin.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
